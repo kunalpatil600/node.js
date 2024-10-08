e2c717d9bbd47550eb1b268dee3c48fa8e769010
kunalpatil600 <kkpatil800@gmail.com>
2024-10-08 14:19:20 +0530

    update

---
+++ Node Js day-1/httpassinment/index.js
@@ -0,0 +1,38 @@
+const http = require("http");
+
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+    if (req.url == '/home') {
+        res.end("<h1>This is Home Page</h1>")
+    } else if (req.url == '/about') {
+        res.end("<h1>This is About Page</h1>")
+    } else if (req.url == '/getproductdata') {
+
+        
+        fs.readFile("./db.json", 'utf-8', (err, data) => {
+            if (err) {
+                console.log(err);
+            } else {
+                const { products } = JSON.parse(data)
+                res.end(JSON.stringify(products))
+            }
+        })
+
+    } else if (req.url == '/user') {
+
+      
+        fs.readFile("./db.json", 'utf-8', (err, data) => {
+            if (err) {
+                console.log(err);
+            } else {
+                const { user } = JSON.parse(data)
+                res.end(JSON.stringify(user))
+            }
+        })
+    }
+})
+
+server.listen(8080, () => {
+    console.log("Server is running on port 8080.")
+})
